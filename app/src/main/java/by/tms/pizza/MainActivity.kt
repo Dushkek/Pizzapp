364658aac8fefd899e14932b128ab0b11d60664d
akelian <exelsage4@gmail.com>
2020-03-16 21:53:20 +0300

    first commit visual interface added

--- app/src/main/java/by/tms/pizza/MainActivity.kt
+++ app/src/main/java/by/tms/pizza/MainActivity.kt
@@ -1,11 +1,36 @@
 package by.tms.pizza
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bavarskayaFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.bavarskayaFragment, bavarskayaFragment).commit()
+
+        val karriFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.karriFragment, karriFragment).commit()
+
+        val pepperoniFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.pepperoniFragment, pepperoniFragment).commit()
+
+        val pineappleFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.pineappleFragment, pineappleFragment).commit()
+
+        val vetchinaFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.vetchinaFragment, vetchinaFragment).commit()
+
+        val tomatnayaFragment = PizzaFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.tomatnayaFragment, tomatnayaFragment).commit()
     }
+
+//    fun fragmentPlacing(v: View?){
+//        val pizzaFragment : PizzaFragment()
+//        supportFragmentManager.beginTransaction().replace(v?.id, pizzaFragment).commit()
+//    }
+
 }
